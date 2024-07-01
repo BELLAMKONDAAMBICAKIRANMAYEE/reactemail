139fe8775baac3bc5b7c009ae301ea29dacaf1fc
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2024-07-01 10:39:20 +0530

    reactwithemailjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-function App() {
+function App () {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_5oniewc', 'template_1zopk7n', form.current, {
+        publicKey: 'Bf2y-ZSpt1oRtigsm',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <h1>ReactJs with EmailJs</h1>
+      <form ref={form} onSubmit={sendEmail} style={{display:"flex",flexDirection:"column",width:"50%",border:"2px solid black",marginTop:"2%",padding:"5%"}}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+       <label>Phone Number</label>
+      <input type="text" name="user_number"/>
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+    </center>
   );
-}
-
+};
 export default App;
